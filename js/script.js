47e359831d1070fff1f02a52add46f0d3f32c7f4
Nimira43 <n.rabone72@aol.com>
2025-06-08 01:43:15 +0100

    Script - 1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const navbar = document.querySelector('.navbar')
+const menuIcon = document.querySelector('.menu-icon')
+const navItems = document.querySelectorAll('.nav-item')
+
+window.addEventListener('scroll', () => {
+
+})
+
+menuIcon.addEventListener('click', () => {
+
+})
+
+navItems.forEach((navItem) => {
+  
+})
